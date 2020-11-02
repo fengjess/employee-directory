eb9e393faac0afef85a2b9ae246fbd01ff313e7e
Jessica Feng <jessicafeng14@yahoo.com>
2020-11-01 17:47:45 -0800

    deployed app

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h3>Employee Directory</h3>
+            <p>Sort by heading or use the search box to narrow your results</p>
+        </header>
+    );
+}
+
+export default Header;
